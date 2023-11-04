7d033f52cea155ce2ea4c80c84856946bdc0502c
MiPiloto <94648941+MiPiloto@users.noreply.github.com>
2023-11-04 11:20:16 -0300

    Menu tests

--- src/components/Menu/stories.tsx
+++ src/components/Menu/stories.tsx
@@ -7,12 +7,33 @@ export default {
   title: 'Menu',
   component: Menu,
   args: mock,
+  parameter: {
+    layout: 'fullscreen',
+  },
 } as Meta<MenuProps>;
 
 export const Template: Story<MenuProps> = (args) => {
   return (
-    <div>
+    <div style={{ maxWidth: '60rem', margin: '5rem auto', padding: '3.2rem' }}>
       <Menu {...args} />
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam non
+        consequatur est recusandae! Ut accusantium reprehenderit delectus porro
+        sint vel exercitationem accusamus odio amet? Cum ab labore qui sit quae.
+      </p>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam non
+        consequatur est recusandae! Ut accusantium reprehenderit delectus porro
+        sint vel exercitationem accusamus odio amet? Cum ab labore qui sit quae.
+      </p>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam non
+        consequatur est recusandae! Ut accusantium reprehenderit delectus porro
+        sint vel exercitationem accusamus odio amet? Cum ab labore qui sit quae.
+      </p>
     </div>
   );
 };
--- src/components/Menu/test.tsx
+++ src/components/Menu/test.tsx
@@ -1,4 +1,4 @@
-import { screen } from '@testing-library/react';
+import { fireEvent, screen } from '@testing-library/react';
 import { renderTheme } from '../../styles/render-theme';
 import { Menu, MenuProps } from '.';
 
@@ -7,9 +7,48 @@ import mock from '../MenuLink/mock';
 const props: MenuProps = mock;
 
 describe('<Menu />', () => {
-  it('should render', () => {
-    renderTheme(<Menu {...props} />);
+  it('should render button link', () => {
+    renderTheme(<Menu {...props} links={undefined} />);
 
-    expect(screen.getByRole('heading', { name: 'Oi' })).toBeInTheDocument();
+    const buttonLink = screen.getByRole('link', {
+      name: 'Open or close menu',
+    });
+    const openMenuIcon = screen.getByLabelText('Open menu');
+
+    expect(buttonLink).toBeInTheDocument();
+    expect(openMenuIcon).toBeInTheDocument();
+
+    expect(screen.queryByLabelText('Close menu')).not.toBeInTheDocument();
+    expect(screen.queryByRole('navigation')).not.toBeInTheDocument();
+  });
+
+  it('should open/close menu on button press', () => {
+    renderTheme(<Menu {...props} links={undefined} />);
+
+    const buttonLink = screen.getByRole('link', {
+      name: 'Open or close menu',
+    });
+
+    expect(buttonLink).toBeInTheDocument();
+    fireEvent.click(buttonLink);
+
+    expect(screen.queryByLabelText('Close menu')).toBeInTheDocument();
+    expect(screen.queryByLabelText('Open menu')).not.toBeInTheDocument();
+    expect(screen.getByRole('navigation')).toBeInTheDocument();
+    expect(screen.getByRole('heading', { name: 'Otavio miranda' }));
+    expect(
+      screen.getByRole('navigation').querySelectorAll('a:not([href="/"])'),
+    ).toHaveLength(mock.links.length);
+
+    fireEvent.click(buttonLink);
+
+    expect(screen.queryByLabelText('Close menu')).not.toBeInTheDocument();
+    expect(screen.queryByRole('navigation')).not.toBeInTheDocument();
+    expect(screen.getByLabelText('Open menu')).toBeInTheDocument();
+  });
+
+  it('should match snapshot', () => {
+    const { container } = renderTheme(<Menu {...props} />);
+    expect(container).toMatchSnapshot();
   });
 });
